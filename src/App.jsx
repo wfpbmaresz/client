3792374ca72ecd86323e2846ed8b3b3ad1b16108
EszterSzollosiMaruzs <maruzseszti@freemail.hu>
2023-10-11 23:51:45 +0200

    Start

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+        {
+          index: true,
+          element: <Landing />,
+        },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
---
+++ src/pages/DashboardLayout.jsx
@@ -0,0 +1,6 @@
+
+const DashboardLayout = () => {
+    return <h1>DashboardLayout</h1>;
+  };
+  
+  export default DashboardLayout;
